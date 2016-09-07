f60a3d3b451c8d3ec063423a35d3d6e42876a216
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-09-06 22:02:13 -0700

    Made responsive and added announcement

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
 (function($) {
-  $('a#about-link').click(function() {
+  $('a.about-link').click(function() {
     $('html, body').animate({
       scrollTop: $('#about-header').offset().top - 32
     }, 500);
   });
 
-  $('a#faq-link').click(function() {
+  $('a.faq-link').click(function() {
     $('html, body').animate({
       scrollTop: $('#faq-header').offset().top - 32
     }, 500);
@@ -14,4 +14,5 @@
 
 $(document).ready(function(){
   $('.parallax').parallax();
+  $('.button-collapse').sideNav();
 });
